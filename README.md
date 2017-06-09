029cb417afc7bd0862ac3390f2d400ad78a9f982
Marc Gagne <marc.gagne@hotmail.com>
2017-06-09 14:11:43 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# SVG 2 UWP Tiles
+
+This utility takes as input an SVG file (preferrably on a transparent background) which will be resized/padded etc. into the various [UWP Tile & Icon assets](https://docs.microsoft.com/en-us/windows/uwp/controls-and-patterns/tiles-and-notifications-app-assets).
+
+But wait, can't you just use the [Auto-generate visual assets for your UWP apps](https://blogs.windows.com/buildingapps/2017/03/07/visual-studio-2017-now-ready-windows-application-development-needs/#ELgkBgKoWGSGx8cs.97) feature found in Visual Studio 2017? YES you can and it's great! I created this for those who don't have/want to install Visual Studio to generate these as well as to provide a little more flexability/customziation, and to learn about how the Logos/Tiles are used.
+
+## Installation
+
+### Save as developer depedency to an existing project
+
+    npm install svg2uwptiles --save-dev
+
+### Install globally
+
+    npm install svg2uwptiles -g
+
+## Usage
+
+    Usage: svg2uwptiles [options]
+
+    Options:
+
+      -h, --help                           output usage information
+      -V, --version                        output the version number
+      -i, --icon-file <path>               Source SVG icon file
+      -c, --canvas-file <path>             Optional canvas (background) file, default: blank.svg
+      -o, --output-folder <path>           Destination folder for generated assets, default: ./assets
+      -n, --names-in-tiles <true|false>    Boolean indicating if names will be included in the tiles which will case padding to be added, default: false
+      -u, --include-unplated <true|false>  Boolean indicating if unplated tiles (for taskbar icons) should be includes, default: true
+
+
+## Examples
+
+### Generate all icons/tiles including unplates tiles for the given SVG file into ./assets
+
+    svg2uwptiles -i myIcon.svg
+
+
+### Generate all icons/tiles including unplates tiles for the given SVG file into a custom output folder
+
+    svg2uwptiles -i myIcon.svg -o "c:\assets"
+
+## Note to Electron users
+
+If using [electron-windows-store](https://github.com/felixrieseberg/electron-windows-store) ensure that your assets folder is **not** under the `input-directory` specified, otherwise you will get errors from `MakePri`.
+
+If using unplated icons (which IMO look better in the taskbar and is the default) you will need to include the argument `--make-pri true` in your `electron-windows-store` call.
+
+Here's an example of my `electron-windows-store` call (as a .cmd)
+
+
+      electron-windows-store ^
+      --identity-name <NAME> ^
+      --package-display-name <NAME> ^
+      --assets "C:\<SOME_DIR>assets" ^
+      --manifest "C:\<SOME_DIR>\AppXManifest.xml" ^
+      --input-directory "C:\<SOME_DIR>\<APP_NAME>-win32-x64" ^
+      --output-directory "C:\<SOME_DIR>\<APP_NAME>-uwp\app" ^
+      --flatten true ^
+      --package-version 1.0.0.0 ^
+      --package-name <EXE_NAME> ^
+      --make-pri true ^
+      --verbose true
+
+## AppxManifest
+
+To ensure the generated icons are used in your UWP application ensure your Logo/Tile names match the ones generated. The following is an example of the `VisualElements` node:
+
+      <uap:VisualElements DisplayName="DemoApp" Square150x150Logo="Assets\Square150x150Logo.png" Square44x44Logo="Assets\Square44x44Logo.png" Description="DemoApp" BackgroundColor="transparent">
+        <uap:LockScreen Notification="badgeAndTileText" BadgeLogo="Assets\BadgeLogo.png" />
+        <uap:DefaultTile Wide310x150Logo="Assets\Wide310x150Logo.png" Square310x310Logo="Assets\LargeTile.png" Square71x71Logo="Assets\SmallTile.png"></uap:DefaultTile>
+        <uap:SplashScreen Image="Assets\SplashScreen.png" />
+      </uap:VisualElements>
---
+++ index.js
@@ -0,0 +1,191 @@
+#! /usr/bin/env node
+
+// Marc Gagne
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+'use strict';
+
+const program = require('commander')
+const fs = require('pn/fs');
+const sharp = require("sharp")
+const pack = require('./package.json')
+
+program
+  .alias('svg2uwptiles')
+  .version(pack.version)
+  .option('-i, --icon-file <path>', 'Source SVG icon file')
+  .option('-c, --canvas-file <path>', 'Optional canvas (background) file, default: blank.svg', 'blank.svg')
+  .option('-o, --output-folder <path>', 'Destination folder for generated assets, default: ./assets', './assets')
+  .option('-n, --names-in-tiles <true|false>', 'Boolean indicating if names will be included in the tiles which will case padding to be added, default: false', false)
+  .option('-u, --include-unplated <true|false>', 'Boolean indicating if unplated tiles (for taskbar icons) should be includes, default: true', true)
+  .parse(process.argv)
+
+if (!program.iconFile) {
+  return program.outputHelp();
+}
+
+let buffers = {
+  canvas: null,
+  overlay: null
+};
+
+
+// UWP Tile/Icon Documentation: https://docs.microsoft.com/en-us/windows/uwp/controls-and-patterns/tiles-and-notifications-app-assets
+
+const assets = {
+  scales: [1, 1.25, 1.5, 2, 4],
+  categories: [
+    { // Small
+      width: 71,
+      height: 71,
+      elementName: "SmallTile",
+      paddingRatioWidth: 0.75,
+      paddingRatioHeight: 0.75
+    },
+    { // Medium
+      width: 150,
+      height: 150,
+      elementName: "MedTile",
+      paddingRatioWidth: !program.namesInTiles ? 0.75 : 0.5,
+      paddingRatioHeight: !program.namesInTiles ? 0.75 : 0.5
+    },
+    { // Wide310x150
+      width: 310,
+      height: 150,
+      elementName: "Wide310x150Logo",
+      paddingRatioWidth: !program.namesInTiles ? 0.75 : 0.5,
+      paddingRatioHeight: !program.namesInTiles ? 0.75 : 0.5
+    },
+    { // Large
+      width: 310,
+      height: 310,
+      elementName: "LargeTile",
+      paddingRatioWidth: !program.namesInTiles ? 0.75 : 0.5,
+      paddingRatioHeight: !program.namesInTiles ? 0.75 : 0.5
+    },
+    { // Splash
+      width: 620,
+      height: 300,
+      elementName: "SplashScreen",
+      //generateNonScaled: true
+    },
+    { // Square44x44
+      width: 44,
+      height: 44,
+      elementName: "Square44x44Logo",
+      targets: [16, 24, 32, 48, 256, 20, 30, 26, 40, 60, 64, 72, 80, 96],
+      unplated: true,
+      paddingRatioWidth: !program.namesInTiles ? 0.75 : 0.5,
+      paddingRatioHeight: !program.namesInTiles ? 0.75 : 0.5
+    },
+    { // Square150x150
+      width: 150,
+      height: 150,
+      elementName: "Square150x150Logo",
+      paddingRatioWidth: !program.namesInTiles ? 0.75 : 0.5,
+      paddingRatioHeight: !program.namesInTiles ? 0.75 : 0.5
+    },
+    { // StoreLogo
+      width: 50,
+      height: 50,
+      elementName: "StoreLogo",
+      generateNonScaled: true,
+      paddingRatioWidth: 0.75,
+      paddingRatioHeight: 0.75
+    }
+  ]
+};
+
+// Verify output folder
+if (!fs.existsSync(program.outputFolder)){
+    fs.mkdirSync(program.outputFolder);
+}
+
+fs.readFile(program.canvasFile)
+  .then((buffer) => {
+    buffers.canvas = buffer;
+    return fs.readFile(program.iconFile)
+  })
+  .then((buffer) => {
+    buffers.overlay = buffer;
+    generateScales()
+  })
+  .then(() => {
+    console.log("Done");
+  });
+
+
+function scaleToSize(outFilename, width, height, paddingRatioWidth, paddingRatioHeight) {
+  width = Math.round(width);
+  height = Math.round(height);
+
+  paddingRatioWidth = paddingRatioWidth ? paddingRatioWidth : 1;
+  paddingRatioHeight = paddingRatioHeight ? paddingRatioHeight : 1;
+
+  return new Promise((resolve, reject) => {
+    let overlayWidth = Math.round(width * paddingRatioWidth);
+    let overlayHeight = Math.round(height * paddingRatioHeight);
+    let left = Math.round(((width - overlayWidth) /  2));
+    let top = Math.round(((height - overlayHeight) /  2));
+
+    console.log("File: '" + outFilename + "', canvas WxH: " + width + "x" + height + ", overlay WxH: " + overlayWidth + "x" + overlayHeight + ", leftxtop: " + left + "x" + top);
+
+    sharp(buffers.overlay)
+      .resize(overlayWidth, overlayHeight)
+      .background({r: 0, g: 0, b: 0, alpha: 0})
+      .embed()
+      .toBuffer()
+      .then((resizedOverlayBuffer) => {
+        sharp(buffers.canvas)
+          .resize(width, height)
+          .background({r: 0, g: 0, b: 0, alpha: 0})
+          .embed()
+          .overlayWith(resizedOverlayBuffer, {left: left, top: top})
+          .toFile(outFilename, function(err) {
+            if (err) {
+              reject(err);
+            }
+            resolve();
+        });
+      })
+  })
+}
+
+function generateScales() {
+  let promises = [];
+
+  console.log("Generating Icons...");
+
+  assets.categories.forEach((category) => {
+    if (category.generateNonScaled) {
+      promises.push(scaleToSize(program.outputFolder + "/" + category.elementName + ".png", category.width, category.height, category.paddingRatioWidth, category.paddingRatioHeight));
+    }
+    assets.scales.forEach((multiplier) => {
+      promises.push(scaleToSize(program.outputFolder + "/" + category.elementName + ".scale-" + (multiplier * 100) + ".png", category.width * multiplier, category.height * multiplier, category.paddingRatioWidth, category.paddingRatioHeight));
+    });
+    if (category.targets) {
+      category.targets.forEach((target) => {
+        promises.push(scaleToSize(program.outputFolder + "/" + category.elementName + ".targetsize-" + target + ".png", target, target, 1, 1));
+
+        if (category.unplated && program.includeUnplated) {
+          promises.push(scaleToSize(program.outputFolder + "/" + category.elementName + ".targetsize-" + target + "_altform-unplated.png", target, target, 1, 1));
+        }
+      });
+    }
+  });
+
+  return new Promise((resolve, reject) => {
+    Promise.all(promises).then(resolve());
+  });
+}
